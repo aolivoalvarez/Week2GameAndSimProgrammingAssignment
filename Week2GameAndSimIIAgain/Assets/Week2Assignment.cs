8fbe6ece17d2bd119acec1f32b5d63f506a9cc0b
roseQuartzUnderTheMoon <alexis.olivo.alvarez@gmail.com>
2021-09-16 11:04:57 -0500

    Function2 Created

--- Week2GameAndSimIIAgain/Assets/Week2Assignment.cs
+++ Week2GameAndSimIIAgain/Assets/Week2Assignment.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Week2Assignment : MonoBehaviour
 {
+    public string sentence = " ";
     // Start is called before the first frame update
     void Start()
     {
         Function1();
+        Function2(sentence);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void Function1()
@@ -21,14 +24,44 @@ public class Week2Assignment : MonoBehaviour
         for (int i = 1; i <= 1000; i++)
         {
             string num = i.ToString();
-            
+
             foreach (char s in num)
             {
                 if ((s == '3') || (s == '7'))
                 {
-                    Debug.Log(i);
+                    Debug.Log(i + " contains a 3 or a 7");
                 }
             }
         }
+    }
+
+    void Function2(string s)
+    {
+        char[] alphabet = new char[] {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+                                           'p','q','r','s','t','u','v','w','x','y','z'};
+        int[] counter = new int[26];
+
+        int largest = 0;
+        char most = ' ';
+
+        foreach (char c in s)
+        {
+            int position = Array.IndexOf(alphabet, c);
+            counter[position]++;
+        }
+
+        foreach (int num in counter)
+        {
+            if (num > largest)
+            {
+                largest = num;
+                int index = Array.IndexOf(counter, largest);
+                most = alphabet[index];
+          
+            }
+      
+        }
+
+        Debug.Log("The letter \"" + most + "\" appears the most in your sentence");
     }
 }
