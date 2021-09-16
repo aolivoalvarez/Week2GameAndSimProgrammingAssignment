f03b596f7d0dafeebf0eff2c3feb7084f6f47342
roseQuartzUnderTheMoon <alexis.olivo.alvarez@gmail.com>
2021-09-15 23:10:44 -0500

    Function 1 Created

--- Week2GameAndSimIIAgain/Assets/Week2Assignment.cs
+++ Week2GameAndSimIIAgain/Assets/Week2Assignment.cs
@@ -7,12 +7,28 @@ public class Week2Assignment : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Function1();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Function1()
+    {
+        for (int i = 1; i <= 1000; i++)
+        {
+            string num = i.ToString();
+            
+            foreach (char s in num)
+            {
+                if ((s == '3') || (s == '7'))
+                {
+                    Debug.Log(i);
+                }
+            }
+        }
     }
 }
